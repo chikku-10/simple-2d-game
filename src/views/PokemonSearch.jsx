adf6c477b4140bf8ad8ed319e7e890000453f1a5
Chikku10 <jiginrajr@gmail.com>
2023-03-01 22:26:47 +0530

    added axios library with api call for pokemon

--- src/views/PokemonSearch.jsx
+++ src/views/PokemonSearch.jsx
@@ -1,26 +1,76 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Box, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import ToastMsg from "../components/ToastMsg";
 
 const PokemonSearch = () => {
   const searchValue = useRef(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [pokemonData, setPokemonData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     searchValue.current = e.target.value;
   };
 
+  const getPokemonData = async () => {
+    try {
+      setLoading(true);
+      if (!searchValue.current) {
+        //show a msg to show
+        setErrorMsg("Field is empty");
+        return;
+      }
+      const responseData = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${searchValue.current.toLowerCase()}`
+      );
+      console.log("responseData", responseData);
+      if (responseData.status === 200) {
+        setPokemonData({name: responseData.data.species.name, url : responseData.data.sprites.front_shiny});
+      } else {
+        setErrorMsg("Failed to get the data. Please try again later");
+        throw new Error("Failed to get the data. Please try again later");
+      }
+    } catch (e) {
+      console.log("error", e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Box display="flex" justifyContent="center">
-      <TextField
-        id="outlined-basic"
-        label="Outlined"
-        variant="outlined"
-        onChange={handleInputChange}
+    <>
+      <ToastMsg
+        open={!!errorMsg}
+        handleClose={() => setErrorMsg(false)}
+        message={errorMsg}
       />
-    <Box mt={2} ml={2}>
-      <SearchIcon />
-    </Box>
-    </Box>
+      <Box display="flex" justifyContent="center" mt={3}>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          onChange={handleInputChange}
+        />
+        <Box mt={2} ml={2}>
+          <SearchIcon sx={{ cursor: "pointer" }} onClick={getPokemonData} />
+        </Box>
+      </Box>
+
+      {loading ? (
+        <Box>Loading</Box>
+      ) : (
+        <Box>
+          {pokemonData && (
+            <Box>
+              <Box>Pokemon Name : {pokemonData.name.toUpperCase()}</Box>
+              <img src={pokemonData.url} alt="pokemon" />
+            </Box>
+          )}
+        </Box>
+      )}
+    </>
   );
 };
 

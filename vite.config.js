df8c6cc75f49ea7b44a35f0d51d247a5b25099f5
Chikku10 <jiginrajr@gmail.com>
2023-03-25 18:57:30 +0530

    changed repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/simple-2D-game'
+  base: "/simple-2d-game"
 })
